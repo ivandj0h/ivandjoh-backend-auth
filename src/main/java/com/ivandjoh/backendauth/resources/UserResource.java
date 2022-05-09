f9e0d676673b5566fe3c26e0919c3450e8a3355a
ivandjoh <ivandi.djoh@gmail.com>
2022-05-09 19:19:09 +0800

    Create Connection to Database and Create packages called UserResource

---
+++ src/main/java/com/ivandjoh/backendauth/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.ivandjoh.backendauth.resources;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserResource {
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody Map<String, Object> userMap) {
+        String firstName = (String) userMap.get("firstName");
+        String lastName = (String) userMap.get("lastName");
+        String email = (String) userMap.get("email");
+        String password = (String) userMap.get("password");
+
+        return firstName + ", " + lastName + ", " + email + ", " + password;
+    }
+}
